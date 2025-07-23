4e1a6acfc651ee7474cf5038a81438ea08186baf
Tblight <duongthanhbinh1411@gmail.com>
2025-07-23 23:35:34 +0700

    Add Chisel(Advanced Item)

--- src/main/java/net/tblight/tutorialmod/item/ModItems.java
+++ src/main/java/net/tblight/tutorialmod/item/ModItems.java
@@ -6,6 +6,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import net.tblight.tutorialmod.item.custom.ChiselItem;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
@@ -16,6 +17,8 @@ public class ModItems {
     public static final RegistryObject<Item> RAW_ALEXANDRITE = ITEMS.register("raw_alexandrite",
             () -> new Item(new Item.Properties()));
 
+    public static final RegistryObject<Item> CHISEL = ITEMS.register("chisel",
+            () -> new ChiselItem(new Item.Properties().durability(32)));
 
 
     public static void register(IEventBus eventBus) {
---
+++ src/main/java/net/tblight/tutorialmod/item/custom/ChiselItem.java
@@ -0,0 +1,64 @@
+package net.tblight.tutorialmod.item.custom;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.tblight.tutorialmod.block.ModBlocks;
+import net.minecraft.core.registries.BuiltInRegistries;
+
+import java.util.Map;
+import java.util.Set;
+
+public class ChiselItem extends Item {
+    public boolean canApplyAtEnchantingTable(ItemStack stack, Enchantment enchantment) {
+        return Items.SHIELD.canApplyAtEnchantingTable(new ItemStack(Items.SHIELD), enchantment);
+    }
+
+    private static final Map<Block, Block> CHISEL_MAP =
+            Map.of(
+                    Blocks.STONE, Blocks.STONE_BRICKS,
+                    Blocks.END_STONE, Blocks.END_STONE_BRICKS,
+                    Blocks.DEEPSLATE, Blocks.DEEPSLATE_BRICKS,
+                    Blocks.IRON_BLOCK, Blocks.DIAMOND_BLOCK,
+                    Blocks.DIRT, ModBlocks.ALEXANDRITE_BLOCK.get()
+            );
+
+    public ChiselItem(Properties pProperties) {
+        super(pProperties);
+    }
+
+
+
+    @Override
+    public InteractionResult useOn(UseOnContext pContext) {
+        Level level = pContext.getLevel();
+        Block clickedBlock = level.getBlockState(pContext.getClickedPos()).getBlock();
+
+        if(CHISEL_MAP.containsKey(clickedBlock)) {
+            if(!level.isClientSide()) {
+                level.setBlockAndUpdate(pContext.getClickedPos(), CHISEL_MAP.get(clickedBlock).defaultBlockState());
+
+                pContext.getItemInHand().hurtAndBreak(1, ((ServerLevel) level), ((ServerPlayer) pContext.getPlayer()),
+                        item -> pContext.getPlayer().onEquippedItemBroken(item, EquipmentSlot.MAINHAND));
+
+                level.playSound(null, pContext.getClickedPos(), SoundEvents.GRINDSTONE_USE, SoundSource.BLOCKS);
+            }
+        }
+
+        return InteractionResult.SUCCESS;
+    }
+}
+
